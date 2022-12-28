2839806d030b5b6687397c304bb44069170d1524
urbancalderon3 <urbancalderon3@gmail.com>
2022-12-27 23:34:24 -0600

    ADDED: create the dates for the daybook

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -12,6 +12,11 @@ export default {
             path: ':id',
             name: 'entry',
             component: () => import(/* webpackChunkName: "daybook-no-entry" */ '@/modules/daybook/views/EntryView'),
+            props: ( route ) => {
+                return {
+                    id: route.params.id
+                }
+            }
         }
     ]
 }
