b1f9f4329698faf89c389bc1d2d97e34b8976e02
urbancalderon3 <urbancalderon3@gmail.com>
2022-12-27 23:32:29 -0600

    ADDED: store module vuex daybook

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -1,7 +1,16 @@
-export const getEntriesByTerm = ( /* state */ ) => {
- /* return state */
+export const getEntriesByTerm = ( state ) => ( term = '' ) => {
+
+    if ( term.length === 0 ) return state.entries
+
+    return state.entries.filter( entry => entry.text.toLowerCase().includes( term.toLocaleLowerCase() ) )
 }
 
-export const getEntryById = ( /* state */ ) => {
- /* return state */
+// id
+export const getEntryById = ( state ) => ( id = '' ) => {
+
+    const entry = state.entries.find( entry => entry.id === id )
+
+    if ( !entry ) return
+
+    return { ...entry }
 }
--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -1,11 +1,21 @@
-export const setEntries = ( /* state */ ) => {
+export const setEntries = ( state, entries ) => {
 
+    // Creamos un nuevo arreglo con los valores anteriores más los nuevos valores
+    state.entries = [...state.entries, ...entries ]
+    state.isLoading = false
 }
 
-export const updateEntry = ( /* state */ ) => {
-
+export const updateEntry = ( state, entry ) => { // entry actualizada
+    const idx = state.entries.map( e => e.id ).indexOf( entry.id )
+    state.entries[idx] = entry
 }
 
-export const addEntry = ( /* state */ ) => {
+export const addEntry = ( state, entry ) => {
+    state.entries = [ entry, ...state.entries ]
+}
 
+export const deleteEntry = ( state, id ) => {
+    
+    state.entries = state.entries.filter( entry => entry.id !== id ) 
+    
 }
