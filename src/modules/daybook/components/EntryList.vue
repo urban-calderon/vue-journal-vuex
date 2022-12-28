f90773d3d3e0fb7979a6d54325e92cd0cf9e88c3
urbancalderon3 <urbancalderon3@gmail.com>
2022-12-27 23:36:27 -0600

    ADDED: create the list entries

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,21 +1,54 @@
 <template>
   <div class="entry-list-container">
-    <div class="px-2 pt-2">
-      <input type="text" class="form-control" placeholder="Buscar entradas" />
+    <div class="px-2 pt-2 pb-2">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Buscar entradas"
+        v-model="term"
+      />
+    </div>
+
+    <div class="mt-2 d-flex flex-column">
+      <button
+        class="btn btn-primary mx-3 mb-3"
+        @click="$router.push({ name: 'entry', params: { id: 'new' } })"
+      >
+        <i class="fa fa-plus-circle"></i>
+        Nueva entrada
+      </button>
     </div>
 
     <div class="entry-scrollarea">
-      <EntryComponent v-for="item in 100" :key="item" />
+      <EntryComponent
+        v-for="entry in entriesByTerm"
+        :key="entry.id"
+        :entry="entry"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { defineAsyncComponent } from "vue";
+import { mapGetters } from "vuex";
 
 export default {
   components: {
     EntryComponent: defineAsyncComponent(() => import("./Entry.vue")),
+  },
+
+  data() {
+    return {
+      term: "",
+    };
+  },
+
+  computed: {
+    ...mapGetters("journal", ["getEntriesByTerm"]),
+    entriesByTerm() {
+      return this.getEntriesByTerm(this.term);
+    },
   },
 };
 </script>
