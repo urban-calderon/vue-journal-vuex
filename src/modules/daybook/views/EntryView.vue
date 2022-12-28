aa63c23cd7bb6bfa956b44a6c484c63b3d6879ef
urbancalderon3 <urbancalderon3@gmail.com>
2022-12-27 23:36:43 -0600

    ADDED: component to display the entry details

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,40 +1,155 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-success fs-3 fw-bold">15</span>
-      <span class="mx-1 fs-3">Julio</span>
-      <span class="mx-2 fs-4 fw-light">2021, jueves</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-3">{{ month }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+      </div>
+
+      <div>
+        <button
+          v-if="entry.id"
+          class="btn btn-danger mx-2"
+          @click="onDeleteEntry"
+        >
+          Borrar
+          <i class="fa fa-trash-alt"></i>
+        </button>
+
+        <button class="btn btn-primary">
+          Subir foto
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
 
-    <div>
-      <button class="btn btn-danger mx-2">
-        Borrar
-        <i class="fa fa-trash-alt"></i>
-      </button>
-
-      <button class="btn btn-primary mx-2">
-        Subir foto
-        <i class="fa fa-upload"></i>
-      </button>
+    <hr />
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea v-model="entry.text" placeholder="¿Qué sucedió hoy?"></textarea>
     </div>
-  </div>
 
-  <hr />
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea placeholder="¿Qué sucedio hoy?"></textarea>
-  </div>
+    <img
+      src="https://www.robertlandscapes.com/wp-content/uploads/2014/11/landscape-322100_1280.jpg"
+      alt="entry-picture"
+      class="img-thumbnail"
+    />
+  </template>
 
-  <Fab icon="fa-save" />
-
-  <img src="@/assets/image.jpg" alt="Entry picture" class="img-thumbnail" />
+  <Fab icon="fa-save" @on:click="saveEntry" />
 </template>
 
 <script>
 import { defineAsyncComponent } from "vue";
+import { mapGetters, mapActions } from "vuex";
+import Swal from "sweetalert2";
+
+import getDayMonthYear from "../helpers/getDayMonthYear";
 
 export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+
   components: {
     Fab: defineAsyncComponent(() => import("../components/Fab.vue")),
+  },
+
+  data() {
+    return {
+      entry: null,
+    };
+  },
+
+  methods: {
+    ...mapActions("journal", ["updateEntry", "createEntry", "deleteEntry"]),
+
+    loadEntry() {
+      let entry;
+
+      if (this.id === "new") {
+        entry = {
+          text: "",
+          date: new Date().getTime(),
+        };
+      } else {
+        entry = this.getEntryById(this.id);
+        if (!entry) return this.$router.push({ name: "no-entry" });
+      }
+
+      this.entry = entry;
+    },
+
+    async saveEntry() {
+      new Swal({
+        title: "Espere por favor",
+        allowOutsideClick: false,
+      });
+      Swal.showLoading();
+
+      if (this.entry.id) {
+        // Actualizar la entrada
+        await this.updateEntry(this.entry);
+      } else {
+        // Crear una nueva entrada
+        const id = await this.createEntry(this.entry);
+
+        this.$router.push({ name: "entry", params: { id } });
+      }
+
+      Swal.fire("Guardado", "Entrada registrada con éxito", "success");
+    },
+
+    async onDeleteEntry() {
+      const { isConfirmed } = await Swal.fire({
+        title: "¿Está seguro?",
+        text: "Una vez borrado, no se puede recuperar",
+        showDenyButton: true,
+        confirmButtonText: "Si, estoy seguro",
+      });
+
+      if (isConfirmed) {
+        new Swal({
+          title: "Espere por favor",
+          allowOutsideClick: false,
+        });
+        Swal.showLoading();
+        await this.deleteEntry(this.entry.id);
+        this.$router.push({ name: "no-entry" });
+
+        Swal.fire("Eliminado", "", "success");
+      }
+    },
+  },
+
+  computed: {
+    ...mapGetters("journal", ["getEntryById"]),
+    day() {
+      const { day } = getDayMonthYear(this.entry.date);
+      return day;
+    },
+    month() {
+      const { month } = getDayMonthYear(this.entry.date);
+      return month;
+    },
+    yearDay() {
+      const { yearDay } = getDayMonthYear(this.entry.date);
+      return yearDay;
+    },
+  },
+
+  created() {
+    // console.log(this.$route.params.id)
+    this.loadEntry();
+  },
+
+  watch: {
+    id() {
+      this.loadEntry();
+    },
   },
 };
 </script>
