1c41278f2d8368554bb98da73fbdc70d160a6652
urbancalderon3 <urbancalderon3@gmail.com>
2023-04-21 09:51:34 -0600

    ADDED: importar la nueva ruta del auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import daybookRouter from "../modules/daybook/router"
+import authRouter from "../modules/auth/router/index"
+
 
 const routes = [
   {
@@ -15,6 +17,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path:'/auth',
+    ...authRouter
   },
   {
     path:'/daybook',
