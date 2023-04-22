848b56b5049edb988e6033de9fb16717e819bd55
urbancalderon3 <urbancalderon3@gmail.com>
2023-04-22 11:13:42 -0600

    ADDED: usar la utenticacion de firebase por medio del token

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -5,4 +5,13 @@ const journalApi = axios.create({
     baseURL: 'https://vue-demos-4072e-default-rtdb.firebaseio.com'
 })
 
+journalApi.interceptors.request.use( (config) => {
+
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+
+    return config
+})
+
 export default journalApi
