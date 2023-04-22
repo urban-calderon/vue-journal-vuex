7b59a35ae5aa167e3e7682206e889282124c9db4
urbancalderon3 <urbancalderon3@gmail.com>
2023-04-22 11:10:55 -0600

    ADDED: funciones para crear un nuevo usuario

--- src/modules/auth/views/Register.vue
+++ src/modules/auth/views/Register.vue
@@ -1,12 +1,16 @@
 <template>
   <span class="login100-form-title p-b-41"> Registro </span>
-  <form class="login100-form validate-form p-b-33 p-t-5">
+  <form
+    class="login100-form validate-form p-b-33 p-t-5"
+    @submit.prevent="onSubmit"
+  >
     <div class="wrap-input100 validate-input" data-validate="Ingrese su nombre">
       <input
         class="input100"
         type="text"
         placeholder="Ingresa tu nombre"
         required
+        v-model="userForm.name"
       />
       <span class="focus-input100" data-placeholder="&#xe82a;"></span>
     </div>
@@ -17,9 +21,10 @@
     >
       <input
         class="input100"
-        type="text"
+        type="email"
         placeholder="Ingresa tu correo electronico"
         required
+        v-model="userForm.email"
       />
       <span class="focus-input100" data-placeholder="&#xe818;"></span>
     </div>
@@ -33,6 +38,7 @@
         type="password"
         placeholder="Contraseña"
         required
+        v-model="userForm.password"
       />
       <span class="focus-input100" data-placeholder="&#xe80f;"></span>
     </div>
@@ -48,5 +54,33 @@
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import useAuth from "../composables/useAuth";
+import Swal from "sweetalert2";
+
+export default {
+  setup() {
+    const router = useRouter();
+    const { createUser } = useAuth();
+
+    const userForm = ref({
+      name: "",
+      email: "",
+      password: "",
+    });
+
+    return {
+      userForm,
+
+      onSubmit: async () => {
+        const { ok, message } = await createUser(userForm.value);
+
+        // comprobamos si no esite el mensaje de error
+        if ((!ok, message)) Swal.fire("Error", message, "error");
+        else router.push({ name: "no-entry" });
+      },
+    };
+  },
+};
 </script>
